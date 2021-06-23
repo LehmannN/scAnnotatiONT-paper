24df99ed585260dc0475748e53baffd2bf7df6f6
Nathalie Lehmann <nathalie.lehmann@bio.ens.psl.eu>
2021-06-23 11:48:14 +0200

    Change data structure

--- _deploy.sh
+++ _deploy.sh
@@ -7,11 +7,11 @@ then
 	echo "You must supply a commit message as an argument"
 	echo "Example command:"
 	echo "   m=\"Your commit message\""
-	echo "   bash _deploy.sh \$m"
+	echo "   bash _deploy.sh \"\$m\""
 	echo
 	exit
 fi
 
-git add *
+git add * || true
 git commit -m "${commitMessage}" || true
-git push origin master
+#git push origin master
